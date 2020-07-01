4d510fc562fd8e756a54ec3f79b6980089d6ab7b
Corteggiani <n.corteggiani@gmail.com>
2020-07-01 17:52:36 +0200

    new ip scripts

---
+++ scan_ip/hdl/scanner.v
@@ -0,0 +1,191 @@
+
+`timescale 1 ns / 1 ps
+
+	module scanner #
+	(
+		parameter integer C_S00_AXI_DATA_WIDTH	= 32,
+		parameter integer C_S00_AXI_ADDR_WIDTH	= 5,
+		parameter integer C_M00_AXI_ADDR_WIDTH	= 32,
+		parameter integer C_M00_AXI_DATA_WIDTH	= 32
+	)
+	(
+		input wire  aclk,
+		input wire  aresetn,
+
+    // scan-chain signals
+    input wire scan_output,
+    output wire scan_ck_enable,
+    output wire scan_input,
+    output wire scan_enable,
+
+    // FIFO 
+    output wire [31:0] data_in_t1,
+    input wire almost_full_t1,
+    input wire empty_t1,
+    output wire rd_en_t1,
+    output wire wr_en_t1,
+    input wire [31:0] data_out_t1,
+
+    input wire empty_t0,
+    output wire [31:0] data_in_t0,
+    input wire almost_full_t0,
+    output wire rd_en_t0,
+    output wire wr_en_t0,
+    input wire [31:0] data_out_t0,
+
+		// Ports of Axi Slave Bus Interface S00_AXI
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+		input wire [2 : 0] s00_axi_awprot,
+		input wire  s00_axi_awvalid,
+		output wire  s00_axi_awready,
+		input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+		input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+		input wire  s00_axi_wvalid,
+		output wire  s00_axi_wready,
+		output wire [1 : 0] s00_axi_bresp,
+		output wire  s00_axi_bvalid,
+		input wire  s00_axi_bready,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+		input wire [2 : 0] s00_axi_arprot,
+		input wire  s00_axi_arvalid,
+		output wire  s00_axi_arready,
+		output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+		output wire [1 : 0] s00_axi_rresp,
+		output wire  s00_axi_rvalid,
+		input wire  s00_axi_rready,
+
+		// Ports of Axi Master Bus Interface M00_AXI
+		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0] m00_axi_awaddr,
+		output wire [2 : 0] m00_axi_awprot,
+		output wire  m00_axi_awvalid,
+		input wire  m00_axi_awready,
+		output wire [C_M00_AXI_DATA_WIDTH-1 : 0] m00_axi_wdata,
+		output wire [C_M00_AXI_DATA_WIDTH/8-1 : 0] m00_axi_wstrb,
+		output wire  m00_axi_wvalid,
+		input wire  m00_axi_wready,
+		input wire [1 : 0] m00_axi_bresp,
+		input wire  m00_axi_bvalid,
+		output wire  m00_axi_bready,
+		output wire [C_M00_AXI_ADDR_WIDTH-1 : 0] m00_axi_araddr,
+		output wire [2 : 0] m00_axi_arprot,
+		output wire  m00_axi_arvalid,
+		input wire  m00_axi_arready,
+		input wire [C_M00_AXI_DATA_WIDTH-1 : 0] m00_axi_rdata,
+		input wire [1 : 0] m00_axi_rresp,
+		input wire  m00_axi_rvalid,
+		output wire  m00_axi_rready
+	);
+
+  wire [31:0] address_src;
+  wire [31:0] address_dst;
+  wire start;
+  wire [15:0] length;
+  wire done;
+
+  wire start;
+  reg start_r;
+  wire start_pulse;
+
+  // wait a pulse to start shifting the scan-chain 
+  always @(posedge aclk)
+      start_r    <= start;
+  
+  assign start_pulse = start_r & ~start;
+
+  // Instantiation of Axi Bus Interface S00_AXI
+  axi_slave # ( 
+		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+	) axi_slave_inst (
+    .address_src(address_src),
+    .address_dst(address_dst),
+    .start(start),
+    .length(length),
+    .done(done),
+		.S_AXI_ACLK(aclk),
+		.S_AXI_ARESETN(aresetn),
+		.S_AXI_AWADDR(s00_axi_awaddr),
+		.S_AXI_AWPROT(s00_axi_awprot),
+		.S_AXI_AWVALID(s00_axi_awvalid),
+		.S_AXI_AWREADY(s00_axi_awready),
+		.S_AXI_WDATA(s00_axi_wdata),
+		.S_AXI_WSTRB(s00_axi_wstrb),
+		.S_AXI_WVALID(s00_axi_wvalid),
+		.S_AXI_WREADY(s00_axi_wready),
+		.S_AXI_BRESP(s00_axi_bresp),
+		.S_AXI_BVALID(s00_axi_bvalid),
+		.S_AXI_BREADY(s00_axi_bready),
+		.S_AXI_ARADDR(s00_axi_araddr),
+		.S_AXI_ARPROT(s00_axi_arprot),
+		.S_AXI_ARVALID(s00_axi_arvalid),
+		.S_AXI_ARREADY(s00_axi_arready),
+		.S_AXI_RDATA(s00_axi_rdata),
+		.S_AXI_RRESP(s00_axi_rresp),
+		.S_AXI_RVALID(s00_axi_rvalid),
+		.S_AXI_RREADY(s00_axi_rready)
+	);
+
+// Instantiation of Axi Bus Interface M00_AXI
+	axi_master # ( 
+		.C_M_AXI_ADDR_WIDTH(C_M00_AXI_ADDR_WIDTH),
+		.C_M_AXI_DATA_WIDTH(C_M00_AXI_DATA_WIDTH)
+	) axi_master_inst (
+		.start(start_pulse),
+    .address_src(address_src),
+    .address_dst(address_dst),
+    .length(length),
+    //FIFO
+    .rd_en(rd_en_t1),
+    .data_in(data_in_t1),
+    .almost_full(almost_full_t1),
+    .wr_en(wr_en_t0),
+    .data_out(data_out_t0),
+    .empty(empty_t0),
+    //AXI Master
+    .M_AXI_ACLK(aclk),
+		.M_AXI_ARESETN(aresetn),
+		.M_AXI_AWADDR(m00_axi_awaddr),
+		.M_AXI_AWPROT(m00_axi_awprot),
+		.M_AXI_AWVALID(m00_axi_awvalid),
+		.M_AXI_AWREADY(m00_axi_awready),
+		.M_AXI_WDATA(m00_axi_wdata),
+		.M_AXI_WSTRB(m00_axi_wstrb),
+		.M_AXI_WVALID(m00_axi_wvalid),
+		.M_AXI_WREADY(m00_axi_wready),
+		.M_AXI_BRESP(m00_axi_bresp),
+		.M_AXI_BVALID(m00_axi_bvalid),
+		.M_AXI_BREADY(m00_axi_bready),
+		.M_AXI_ARADDR(m00_axi_araddr),
+		.M_AXI_ARPROT(m00_axi_arprot),
+		.M_AXI_ARVALID(m00_axi_arvalid),
+		.M_AXI_ARREADY(m00_axi_arready),
+		.M_AXI_RDATA(m00_axi_rdata),
+		.M_AXI_RRESP(m00_axi_rresp),
+		.M_AXI_RVALID(m00_axi_rvalid),
+		.M_AXI_RREADY(m00_axi_rready)
+	);
+
+  scan scan_inst(
+    // Common Signals
+	  .aclk(aclk),
+	  .aresetn(aresetn),
+    // FIFO
+    .rd_en(rd_en_t0),
+    .data_in(data_in_t0),
+    .almost_full(almost_full_t0),
+    .wr_en(wr_en_t1),
+    .data_out(data_out_t1),
+    .empty(empty_t1),
+    // User-configuration
+    .start(start),
+    .length(length),
+    .done(done),
+    // Scan Signals
+    .scan_output(scan_output),
+    .scan_input(scan_input),
+    .scan_enable(scan_enable),
+    .scan_ck_enable(scan_ck_enable)
+  );
+	// User logic ends
+
+	endmodule
---
+++ scan_ip/tcl/simu.tcl
@@ -0,0 +1,110 @@
+#
+# Author  : Corteggiani Nassim
+# Company : EURECOM
+# DATA    : 2019
+
+proc usage {} {
+	puts "usage: vivado -mode batch -source <script> -tclargs <rootdir> <builddir>"
+	puts "  <rootdir>:  absolute path of usb2jtag root directory"
+	puts "  <builddir>: absolute path of build directory"
+	exit -1
+}
+
+if { $argc == 2 } {
+	set rootdir [lindex $argv 0]
+	set builddir [lindex $argv 1]
+} else {
+	usage
+}
+
+cd $builddir
+
+###################
+# Create DISPATCHER 
+###################
+create_project -part xc7z020clg484-1 -force t0 t0_ip
+set sources [glob -directory ../../hdl *.v]
+foreach f $sources {
+        add_files $builddir/$f
+}
+import_files -force -norecurse
+ipx::package_project -root_dir t0_ip -vendor www.eurecom.fr -library ip -force t0
+close_project
+
+set top top
+create_project -part xc7z020clg484-1 -force $top .
+set_property board_part em.avnet.com:zed:part0:1.4 [current_project]
+set_property ip_repo_paths { ./t0_ip } [current_fileset]
+update_ip_catalog
+create_bd_design "$top"
+
+create_bd_cell -type ip -vlnv www.eurecom.fr:ip:scanner:1.0 t0
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_vip:1.1 axi_vip_0
+create_bd_cell -type ip -vlnv xilinx.com:ip:fifo_generator:13.2 fifo_generator_t0
+create_bd_cell -type ip -vlnv xilinx.com:ip:fifo_generator:13.2 fifo_generator_t1
+
+set_property -dict [list CONFIG.PROTOCOL {AXI4LITE}] [get_bd_cells axi_vip_0]
+set_property -dict [list CONFIG.PROTOCOL.VALUE_SRC USER] [get_bd_cells axi_vip_0]
+set_property -dict [list CONFIG.PROTOCOL {AXI4LITE} CONFIG.INTERFACE_MODE {PASS_THROUGH}] [get_bd_cells axi_vip_0]
+
+set_property -dict [list CONFIG.Input_Data_Width {32} CONFIG.Output_Data_Width {32} CONFIG.Reset_Pin {false} CONFIG.Reset_Type {Asynchronous_Reset} CONFIG.Use_Dout_Reset {false} CONFIG.Almost_Full_Flag {true}] [get_bd_cells fifo_generator_t0]
+set_property -dict [list CONFIG.Input_Data_Width {32} CONFIG.Output_Data_Width {32} CONFIG.Reset_Pin {false} CONFIG.Reset_Type {Asynchronous_Reset} CONFIG.Use_Dout_Reset {false} CONFIG.Almost_Full_Flag {true}] [get_bd_cells fifo_generator_t1]
+
+create_bd_port -dir O -type clk aclk
+create_bd_port -dir O -type reset aresetn
+
+create_bd_port -dir I scan_output
+create_bd_port -dir O scan_input
+create_bd_port -dir O scan_ck_enable
+create_bd_port -dir O scan_enable
+
+connect_bd_net [get_bd_ports scan_output] [get_bd_pins t0/scan_output]
+connect_bd_net [get_bd_ports scan_ck_enable] [get_bd_pins t0/scan_ck_enable]
+connect_bd_net [get_bd_ports scan_input] [get_bd_pins t0/scan_input]
+connect_bd_net [get_bd_ports scan_enable] [get_bd_pins t0/scan_enable]
+
+connect_bd_net [get_bd_ports aclk] [get_bd_pins t0/aclk]
+connect_bd_net [get_bd_ports aresetn] [get_bd_pins t0/aresetn]
+connect_bd_net [get_bd_ports aclk] [get_bd_pins axi_vip_0/aclk]
+connect_bd_net [get_bd_ports aresetn] [get_bd_pins axi_vip_0/aresetn]
+connect_bd_net [get_bd_ports aclk] [get_bd_pins fifo_generator_t0/clk]
+connect_bd_net [get_bd_ports aclk] [get_bd_pins fifo_generator_t1/clk]
+
+connect_bd_intf_net [get_bd_intf_pins axi_vip_0/M_AXI] [get_bd_intf_pins t0/s00_axi]
+connect_bd_intf_net [get_bd_intf_pins axi_vip_0/S_AXI] [get_bd_intf_pins t0/m00_axi]
+
+connect_bd_net [get_bd_pins fifo_generator_t0/almost_full] [get_bd_pins t0/almost_full_t0]
+connect_bd_net [get_bd_pins fifo_generator_t0/din] [get_bd_pins t0/data_in_t0]
+connect_bd_net [get_bd_pins fifo_generator_t0/wr_en] [get_bd_pins t0/wr_en_t0]
+connect_bd_net [get_bd_pins fifo_generator_t0/empty] [get_bd_pins t0/empty_t0]
+connect_bd_net [get_bd_pins fifo_generator_t0/rd_en] [get_bd_pins t0/rd_en_t0]
+connect_bd_net [get_bd_pins fifo_generator_t0/dout] [get_bd_pins t0/data_out_t0]
+
+connect_bd_net [get_bd_pins fifo_generator_t1/almost_full] [get_bd_pins t0/almost_full_t1]
+connect_bd_net [get_bd_pins fifo_generator_t1/din] [get_bd_pins t0/data_in_t1]
+connect_bd_net [get_bd_pins fifo_generator_t1/wr_en] [get_bd_pins t0/wr_en_t1]
+connect_bd_net [get_bd_pins fifo_generator_t1/empty] [get_bd_pins t0/empty_t1]
+connect_bd_net [get_bd_pins fifo_generator_t1/rd_en] [get_bd_pins t0/rd_en_t1]
+connect_bd_net [get_bd_pins fifo_generator_t1/dout] [get_bd_pins t0/data_out_t1]
+
+assign_bd_address [get_bd_addr_segs {axi_vip_0/S_AXI/Reg }]
+set_property offset 0x00000000 [get_bd_addr_segs {axi_vip_0/Master_AXI/SEG_t0_reg0}]
+set_property range 4G [get_bd_addr_segs {axi_vip_0/Master_AXI/SEG_t0_reg0}]
+
+make_wrapper -files [get_files $builddir/top.srcs/sources_1/bd/top/top.bd] -top
+add_files -norecurse $builddir/top.srcs/sources_1/bd/top/hdl/top_wrapper.v
+
+set_property SOURCE_SET sources_1 [get_filesets sim_1]
+add_files -fileset sim_1 -norecurse $builddir/../tb/testbench.sv
+update_compile_order -fileset sim_1
+
+save_bd_design
+
+generate_target Simulation [get_files $builddir/top.srcs/sources_1/bd/top/top.bd]
+export_ip_user_files -of_objects [get_files $builddir/top.srcs/sources_1/bd/top/top.bd] -no_script -sync -force -quiet
+export_simulation -of_objects [get_files $builddir/top.srcs/sources_1/bd/top/top.bd] -directory $builddir/top.ip_user_files/sim_scripts -ip_user_files_dir $builddir/top.ip_user_files -ipstatic_source_dir $builddir/top.ip_user_files/ipstatic -lib_map_path [list {modelsim=$builddir/top.cache/compile_simlib/modelsim} {questa=$builddir/top.cache/compile_simlib/questa} {ies=$builddir/top.cache/compile_simlib/ies} {xcelium=$builddir/top.cache/compile_simlib/xcelium} {vcs=$builddir/top.cache/compile_simlib/vcs} {riviera=$builddir/top.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+
+launch_simulation
+
+#start_gui
+
